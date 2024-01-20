245a851bee2f0a66db5b8a8194bf6b429273d259
unknown <rafaelky.dev@gmail.com>
2024-01-20 17:07:07 +0800

    added bottom tabs

--- assets/Components/homeScreen.js
+++ assets/Components/homeScreen.js
@@ -1,17 +1,37 @@
-import { StatusBar } from "react-native";
-import { Button, StyleSheet, View } from "react-native"
+import { StatusBar, Button, StyleSheet, View, Text } from "react-native"
 import { SafeAreaView } from "react-native-safe-area-context";
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import MapView from 'react-native-maps';
+
+const Tab = createBottomTabNavigator();
 
 export function HomeScreen({navigation}){
     return (
-        <SafeAreaView style={styles.container}>
-            <StatusBar 
-            hidden= {true}/>
-            <MapView width="100%" height="100%"/>
-        
-        </SafeAreaView>
+        <>
+        <Tab.Navigator>
+            <Tab.Screen name="Map" component={MapScreen} options={{ headerShown: false }}/>
+            <Tab.Screen name="Settings" component={SettingsScreen} />
+        </Tab.Navigator>
+        <StatusBar hidden= {true}/>
+        </>
     );
+}
+
+function MapScreen() {
+    return (
+        <SafeAreaView style={styles.container}>
+            <MapView width="100%" height="100%"/>
+        </SafeAreaView>
+    )
+    
+}
+
+function SettingsScreen() {
+  return (
+    <SafeAreaView style={styles.container}>
+        <Text>Some preferences</Text>
+    </SafeAreaView>
+  );
 }
 
 const styles = StyleSheet.create({

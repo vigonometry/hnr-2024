06495b7cc68be9fc89ec9e9dd7e8de6e12e1a59e
Clifong <59185915+Clifong@users.noreply.github.com>
2024-01-21 00:14:29 +0800

    tweak bottom bar

--- assets/Components/homeScreen.js
+++ assets/Components/homeScreen.js
@@ -1,8 +1,9 @@
-import { StatusBar, Button, StyleSheet, View, Text } from "react-native"
+import { StatusBar, Button, StyleSheet, View, Text, Image } from "react-native"
 import { SafeAreaView } from "react-native-safe-area-context";
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { MapScreen } from "./Tabs/mapScreen";
 import { SettingsScreen } from "./Tabs/settingsScreen";
+import { CustomHomeHeader } from "./Headers/header_home";
 
 const Tab = createBottomTabNavigator();
 
@@ -10,8 +11,8 @@ export function HomeScreen({navigation}){
     return (
         <>
         <Tab.Navigator>
-            <Tab.Screen name="Map" component={MapScreen} options={{ headerShown: false }}/>
-            <Tab.Screen name="Settings" component={SettingsScreen} />
+            <Tab.Screen name="Map" component={MapScreen} options={{tabBarIcon: () => <Image height={20} width={20} source={{uri:"https://cdn-icons-png.flaticon.com/128/447/447031.png"}}/>}}/>
+            <Tab.Screen name="Settings" component={SettingsScreen} options={{tabBarIcon: () => <Image height={20} width={20} source={{uri:"https://cdn-icons-png.flaticon.com/128/484/484613.png"}}/>}}/>
         </Tab.Navigator>
         <StatusBar hidden= {true}/>
         </>

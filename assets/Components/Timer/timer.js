0c82d47a7de41350024db7811cc0db1194ebc8f8
Vigneshwar Hariharan <vigneshwar.hariharan1@gmail.com>
2024-01-21 11:33:40 +0800

    add timer

--- assets/Components/Timer/timer.js
+++ assets/Components/Timer/timer.js
@@ -1,34 +1,38 @@
-import React from 'react';
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import React from "react";
+import { View, TouchableOpacity, StyleSheet } from "react-native";
+import { Icon, Text } from "react-native-paper";
 
 export default class Timer extends React.Component {
-
-    state = {
-        time: 100,
-      };
-    
-    render() {
-    return (
-        <View>
-            <Text>{this.state.time}</Text>
-        </View>
-    );
-    }
-
-    componentDidMount() {
-        this.startTimer();
-    }
-
-    startTimer = () => {
-        this.interval = setInterval(() => {
-        this.setState(state => ({
-        time: state.time - 1,
-        }));
-    }, 1000);
+  constructor(props) {
+    super(props);
+    this.state = {
+      time: props.time,
     };
-    
-    //If the timer stops
-    // stopTimer = () => {
-    //     clearInterval(this.interval);
-    //   };
   }
+
+  render() {
+    return (
+      <View>
+        {this.state.time >= 0 ? <Text variant="displayLarge">{this.state.time}</Text> : <Icon source="emoticon-sad" width="50%"/>}
+        {this.state.time <= 120 ? <Text variant="displayLarge" style={{color: "red"}}>RUN</Text> : <></>}
+      </View>
+    );
+  }
+
+  componentDidMount() {
+    this.startTimer();
+  }
+
+  startTimer = () => {
+    this.interval = setInterval(() => {
+      this.setState((state) => ({
+        time: state.time - 1,
+      }));
+    }, 1000);
+  };
+
+  //If the timer stops
+  // stopTimer = () => {
+  //     clearInterval(this.interval);
+  //   };
+}
